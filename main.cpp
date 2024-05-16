75517e1c4fc453cf7b2e1e3260f08f8d789bf8f0
yahia <y_kilany@aucegypt.edu>
2024-05-16 20:25:41 +0300

    split the header file into a .h and a .cpp file and added functionality to calculate time

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "HuffmanCoding.h" // Include the header file for HuffmanCoding class
-
+#include <chrono>
+using namespace std::chrono;
 int main() {
     HuffmanCoding huffman;
 
@@ -14,8 +15,16 @@ int main() {
     cin >> decompressedFile;
 
     // Compress the input file and then decompress it
+    auto compress_start = high_resolution_clock::now();
     huffman.compressFile(inputFile, compressedFile);
+    auto compress_stop = high_resolution_clock::now();
+    auto decompress_start = high_resolution_clock::now();
     huffman.decompressFile(compressedFile, decompressedFile);
+    auto decompress_stop = high_resolution_clock::now();
+    auto compress_duration = duration_cast<microseconds>(compress_stop - compress_start);
+    auto decompress_duration = duration_cast<microseconds>(decompress_stop - decompress_start);
+    cout << "Compression time: " << compress_duration.count() << " microseconds" << endl;
+    cout << "Decompression time: " << decompress_duration.count() << " microseconds" << endl;
 
     return 0;
 }
